8df4a4b6a5a264cd3b301c43edebbf8ea1dcbacc
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-20 11:03:09 +0300

    adding some content and a form for new quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,4 +1,25 @@
+import QuoteList from "../components/quotes/QuoteList";
+import { Fragment } from "react";
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  const DUMMY_QUOTES = [
+    {
+      id: "q1",
+      author: "Oscar Wilde",
+      text: "Be yourself, everybody else is taken.",
+    },
+    {
+      id: "q2",
+      author: "Albert Einstein",
+      text: "There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.",
+    },
+  ];
+
+  return (
+    <Fragment>
+      <h1>All Quotes</h1>
+      <QuoteList quotes={DUMMY_QUOTES} />
+    </Fragment>
+  );
 };
 export default AllQuotes;
