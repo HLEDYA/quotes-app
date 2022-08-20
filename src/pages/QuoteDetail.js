4db60e034614dfedf45d225b7af8a0be305ad8b8
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-20 15:58:04 +0300

    outputting data on the details page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,33 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Oscar Wilde",
+    text: "Be yourself, everybody else is taken.",
+  },
+  {
+    id: "q2",
+    author: "Albert Einstein",
+    text: "There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.qid);
+  if (!quote) {
+    return <p>No Quote found!</p>;
+  }
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <p1>{params.qid}</p1>
-      <Route path={`/quotes/${params.qid}/comments`}>
+      {/* <HighlightedQuote text={quote.text} author={quote.author} /> */}
+      <h1>{quote.text}</h1>
+      <p1>{quote.author}</p1>
+      <Route path={`/quotes/${quote.id}/comments`}>
         <Comments />
       </Route>
     </Fragment>
