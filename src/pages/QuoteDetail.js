d515cdc0597d5c291966b12a280cefa37e28b37b
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-21 07:00:06 +0300

    loading comments from quote highlight page by using nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Route, useParams, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -27,6 +27,15 @@ const QuoteDetail = () => {
       {/* <HighlightedQuote text={quote.text} author={quote.author} /> */}
       <h1>{quote.text}</h1>
       <p1>{quote.author}</p1>
+
+      <Route path={`/quotes/${quote.id}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${quote.id}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
       <Route path={`/quotes/${quote.id}/comments`}>
         <Comments />
       </Route>
